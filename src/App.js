91424c0ba4e0389985f5e3f4f93ba5ecea060c9d
Joel Yek Zong Yong <joel.yekzongyong@Imp.net>
2019-08-07 16:40:53 +0800

    added materia ui, redux, redux form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
 import logo from './logo.svg';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+const rootReducer = combineReducers({
+  // ...your other reducers here
+  // you have to pass formReducer under 'form' key,
+  // for custom keys look up the docs for 'getFormState'
+  form: formReducer
+})
+
+const store = createStore(rootReducer)
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and Test to reload.
         </p>
+        <Button variant="contained" color="primary" href="#contained-buttons" className={classes.button}>
+          Link
+      </Button>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          multiple
+          type="file"
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" component="span" className={classes.button}>
+            Upload
+        </Button>
+        </label>
         <a
           className="App-link"
           href="https://reactjs.org"
